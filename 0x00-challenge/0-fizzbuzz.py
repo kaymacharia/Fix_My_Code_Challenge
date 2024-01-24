b396739427ca43d22e5f68334022337889432de1
kaymacharia <kaymachariaa@gmail.com>
2024-01-24 05:35:11 -0800

    1

--- 0x00-challenge/0-fizzbuzz.py
+++ 0x00-challenge/0-fizzbuzz.py
@@ -1,39 +1,23 @@
-#!/usr/bin/python3
-""" FizzBuzz
-"""
 import sys
 
-
 def fizzbuzz(n):
-    """
-   The FizzBuzz function outputs numbers from 1 to n, with each number separated by a space. It follows the rules:
-
-	If a number is a multiple of three, it prints "Fizz" instead of the number.
-	If a number is a multiple of five, it prints "Buzz" instead of the number.
-	If a number is a multiple of both three and five, it prints "FizzBuzz".
-    """
-    if n < 1:
-        return
-
-    tmp_result = []
     for i in range(1, n + 1):
-        if (i % 3) == 0 and (i % 5) == 0:
-            tmp_result.append("FizzBuzz")
-        elif (i % 3) == 0:
-            tmp_result.append("Fizz")
-        elif (i % 5) == 0:
-            tmp_result.append("Buzz")
-        else:
-            tmp_result.append(str(i))
-    print(" ".join(tmp_result))
+        output = ""
+        if i % 3 == 0:
+            output += "Fizz"
+        if i % 5 == 0:
+            output += "Buzz"
+        if not output:
+            output = i
+        print(output, end=" ")
 
-
-if __name__ == '__main__':
-    if len(sys.argv) <= 1:
-        print("Missing number")
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
         print("Usage: ./0-fizzbuzz.py <number>")
-        print("Example: ./0-fizzbuzz.py 89")
         sys.exit(1)
 
-    number = int(sys.argv[1])
-    fizzbuzz(number)
+    try:
+        n = int(sys.argv[1])
+        fizzbuzz(n)
+    except ValueError:
+        print("Please provide a valid number.")
